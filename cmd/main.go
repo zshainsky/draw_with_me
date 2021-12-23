f2f0891b63177a79117f59cb02ff7e8ec1629294
Zack Shainsky <zshainsky@gmail.com>
2021-12-23 15:13:09 -0800

    updated main to read port from environment var

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 	"github.com/zshainsky/draw-with-me"
@@ -23,8 +24,12 @@ func main() {
 
 	router = mux.NewRouter()
 	draw.NewServer(router)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "3000" // Default port if not specified
+	}
 
-	err := http.ListenAndServe(":3000", router)
+	err := http.ListenAndServe(":"+port, router)
 	if err != nil {
 		log.Fatal(err)
 	}
